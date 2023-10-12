afd3415cf7c76d2cbf51660f84ea2b1caac6a0a2
ADE OLAOYE <adeolaoye56@gmail.com>
2023-10-12 19:51:28 +0100

    make portfolio responsive

--- app/components/Skills.js
+++ app/components/Skills.js
@@ -1,5 +1,16 @@
 import React from 'react'
-import { AiFillHtml5 } from "react-icons/ai";
-import { SiCss3 } from "react-icons/si";
+import { SiCss3, SiJavascript } from "react-icons/si";
 import { BsFillBootstrapFill } from "react-icons/bs";
-import { FaReact } from "react-icons/fa";
+import { FaReact, FaHtml5 } from "react-icons/fa";
+
+export default function Skills() {
+    return (
+        <section className='skills-wrapper' id='skills'>
+            <FaHtml5 className='skills-icon' />
+            <SiCss3 className='skills-icon' />
+            <SiJavascript className='skills-icon' />
+            <BsFillBootstrapFill className='skills-icon' />
+            <FaReact className='skills-icon' />
+        </section>
+    )
+}
